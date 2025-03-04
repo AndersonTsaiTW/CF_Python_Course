9dd7898423f6d0a250e7a33dd05b2f4a2544bb5e
AndersonTsaiTW <AndersonTsaiTW@gmail.com>
2025-03-03 22:41:36 -0500

    Finished Ex2.5 including some documents, reflection, and screenshot

--- Achievement_02/src_practice_bookshelf/books/views.py
+++ Achievement_02/src_practice_bookshelf/books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render  # imported by default
+# to display lists and details  # to display lists
+from django.views.generic import ListView, DetailView
+from .models import Book  # to access Book model
 
 # Create your views here.
+
+
+class BookListView(ListView):  # class-based view
+    model = Book  # specify model
+    template_name = 'books/main.html'  # specify template
+
+
+class BookDetailView(DetailView):  # class-based view
+    model = Book  # specify model
+    template_name = 'books/detail.html'  # specify template
